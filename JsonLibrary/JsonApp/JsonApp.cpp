70edfe86a4e5f84a39c3062459d1f9e29fddb9bd
lev-michael <michael.lev@hrsd.cz>
2020-12-28 19:41:29 +0100

    sem complete

--- JsonLibrary/JsonApp/JsonApp.cpp
+++ JsonLibrary/JsonApp/JsonApp.cpp
@@ -1,13 +1,6 @@
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <crtdbg.h>
-
-#ifdef _DEBUG
-#define new new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
-#else
-#define new new
-#endif
-
 #include "App.h"
 
 
